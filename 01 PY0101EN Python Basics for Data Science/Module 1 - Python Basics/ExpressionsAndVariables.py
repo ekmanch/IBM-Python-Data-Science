5d1d9d6ecc5dab185af36d47a721aed88b31d56f
ekmanch <christian.ekman89@gmail.com>
2021-01-16 19:55:17 +0100

    changed 2 of the folder names in the folder structure

---
+++ 01 PY0101EN Python Basics for Data Science/Module 1 - Python Basics/ExpressionsAndVariables.py
@@ -0,0 +1,1 @@
+# Will use this file for the Expressions And Variables lab
---
+++ 01 PY0101EN Python Basics for Data Science/Module 1 - Python Basics/StringOperations.py
@@ -0,0 +1,15 @@
+# Use slicing to find the first four elements of the following string: Letters
+
+Letters="ABCDEFGHIJK"
+
+print(Letters[0:4])
+
+# Use a stride value of two on the following string: Good
+
+Good="GsoAo+d"
+
+print(Good[::2])
+
+# Convert the following string to uppercase using the method upper(): "uppercase"
+
+print("uppercase".upper())
