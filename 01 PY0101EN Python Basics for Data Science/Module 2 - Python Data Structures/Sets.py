8f71d954f7fad7401c3b63b950e41df1c8029816
ekmanch <christian.ekman89@gmail.com>
2021-01-23 22:46:15 +0100

    Did the first Sets lab, as well as the first Sets quiz

---
+++ 01 PY0101EN Python Basics for Data Science/Module 2 - Python Data Structures/Sets.py
@@ -0,0 +1,46 @@
+######################################
+# Sets Lab
+######################################
+
+# Cast the following list to a set:
+
+print(set(['A','B','C','A','B','C']))
+
+# Add the string 'D' to the set S
+
+S={'A','B','C'}
+S.add('D')
+print(S)
+
+# Find the intersection of set A and B
+
+A={1,2,3,4,5}
+B={1,3,9, 12}
+
+print(A & B)
+
+######################################
+# Sets Quiz
+######################################
+
+# Question 1:
+# What is the result of the following lines of code:
+
+S={'A','B','C'}
+
+U={'A','Z','C'}
+
+print(U.union(S))
+
+# Question 2:
+# What is the intersection of set S and U
+
+S={'A','B','C'}
+
+U={'A','Z','C'}
+
+print(S&U)
+
+######################################
+# Sets in Python Lab 
+######################################
