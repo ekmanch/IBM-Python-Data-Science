cbabb8e4dd7a47ce2144196197aabdbb54e8e51b
ekmanch <christian.ekman89@gmail.com>
2021-01-23 23:19:52 +0100

    Finished the first Dictionaries Lab

---
+++ 01 PY0101EN Python Basics for Data Science/Module 2 - Python Data Structures/Dictionaries.py
@@ -0,0 +1,23 @@
+######################################
+# Dictionaries Lab 1
+######################################
+
+# You will need the dictionary D:
+
+from typing import Dict
+
+
+D={'a':0,'b':1,'c':2}
+
+# Find the value for the key  'a'
+
+print(D['a'])
+
+# Find the keys of the dictionary D
+
+print(D.keys())
+
+######################################
+# Dictionaries Quiz
+######################################
+
