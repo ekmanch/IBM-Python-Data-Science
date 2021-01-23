76bb5d80b32a31b18f235be173bf61f43e274f9c
ekmanch <christian.ekman89@gmail.com>
2021-01-23 23:24:25 +0100

    Finished the Dictionaries quiz

--- 01 PY0101EN Python Basics for Data Science/Module 2 - Python Data Structures/Dictionaries.py
+++ 01 PY0101EN Python Basics for Data Science/Module 2 - Python Data Structures/Dictionaries.py
@@ -21,3 +21,21 @@ print(D.keys())
 # Dictionaries Quiz
 ######################################
 
+# Question 1
+# Consider the following dictionary:
+D={'a':0,'b':1,'c':2}
+# What is the result of the following: D.values()
+
+# Answer prediction:
+# answer will be a list of values in D, i.e. 0, 1 and 2
+
+print(D.values())
+
+# Question 2
+# Consider the following dictionary:
+D={'a':0,'b':1,'c':2}
+# What is the output of the following D['b'] :
+
+# Answer prediction:
+# answer will be the value of key 'b', i.e. 1
+print(D['b'])
