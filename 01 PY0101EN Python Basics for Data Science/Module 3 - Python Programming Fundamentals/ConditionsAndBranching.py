989fa56e7012790d3dad491fee08941b31000e08
ekmanch <christian.ekman89@gmail.com>
2021-01-24 16:30:55 +0100

    Finished the Conditions and Branching Quick Practice Lab

---
+++ 01 PY0101EN Python Basics for Data Science/Module 3 - Python Programming Fundamentals/ConditionsAndBranching.py
@@ -0,0 +1,29 @@
+############################################
+# ConditionsAndBranching Quick Practice Lab
+############################################
+
+# Find the value of i that produces a True
+# i = ?
+# i!=0
+i=1
+print(i!=0)
+
+# Find the value of x that prints the statement: "this is a"
+# x = ?
+# if(x=='a'):
+#    print("this is a")
+# else:
+#     print("this is  not a")
+x='a'
+if(x=='a'):
+    print("this is a")
+else:
+    print("this is  not a")
+
+# find the value of y that produces a True statement
+# y=
+# x=1
+# x>0 and y<10
+y=9
+x=1
+print(x>0 and y<10)
