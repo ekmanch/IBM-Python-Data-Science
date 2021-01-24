e55af11449328b64419581cc927088d91285ac15
ekmanch <christian.ekman89@gmail.com>
2021-01-24 17:06:03 +0100

    Finished the Conditions and Branching Lab

--- 01 PY0101EN Python Basics for Data Science/Module 3 - Python Programming Fundamentals/ConditionsAndBranching.py
+++ 01 PY0101EN Python Basics for Data Science/Module 3 - Python Programming Fundamentals/ConditionsAndBranching.py
@@ -67,3 +67,33 @@ else:
 # ConditionsAndBranching Lab
 ############################################
 
+# Question 1
+# Write an if statement to determine if an album had a rating greater than 8.
+# Test it using the rating for the album “Back in Black”
+# that had a rating of 8.5.
+# If the statement is true print "This album is Amazing!"
+
+album = 8.5
+
+if (album > 8):
+    print("This album is Amazing!")
+
+# Question 2
+# Write an if-else statement that performs the following.
+# If the rating is larger then eight print “this album is amazing”.
+# If the rating is less than or equal to 8 print “this album is ok”.
+
+if (album > 8):
+    print("This album is Amazing!")
+else:
+    print("This album is ok")
+
+# Question 3
+# Write an if statement to determine if an album came out before 1980
+# or in the years: 1991 or 1993.
+# If the condition is true print out the year the album came out.
+
+release_year = 1993
+
+if (release_year < 1980) or (release_year == 1991) or (release_year == 1993):
+    print("The album was released in: ", release_year)
