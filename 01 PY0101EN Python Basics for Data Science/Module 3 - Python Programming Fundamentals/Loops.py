bf18917b68fc1a8b82ef9a8419f7e22b80944cf7
ekmanch <christian.ekman89@gmail.com>
2021-01-24 17:27:37 +0100

    Added jupyter notebook for Loops lab and finished the Loops Quick Practice Lab

--- 01 PY0101EN Python Basics for Data Science/Module 3 - Python Programming Fundamentals/Loops.py
+++ 01 PY0101EN Python Basics for Data Science/Module 3 - Python Programming Fundamentals/Loops.py
@@ -2,3 +2,27 @@
 # Loops Quick Practice Lab
 ############################################
 
+# Use loops to print out the elements in the list A
+# A=[3,4,5]
+
+print("QUESTION 1")
+
+A=[3,4,5]
+
+for i in A:
+    print(i)
+
+# Find the value of  x  that will print out the sequence  1,2,..,10 
+# x=
+# y=1
+# while(y != x):
+#     print(y)
+#     y=y+1
+
+print("QUESTION 2")
+
+x=11
+y=1
+while(y != x):
+    print(y)
+    y=y+1
