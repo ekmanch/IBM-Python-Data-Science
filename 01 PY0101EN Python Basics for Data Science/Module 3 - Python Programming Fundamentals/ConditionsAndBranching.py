a50ecfdf7af7819d0980158b4f4f498440d294de
ekmanch <christian.ekman89@gmail.com>
2021-01-24 16:38:31 +0100

    Finished the Conditions and Branching quiz

--- 01 PY0101EN Python Basics for Data Science/Module 3 - Python Programming Fundamentals/ConditionsAndBranching.py
+++ 01 PY0101EN Python Basics for Data Science/Module 3 - Python Programming Fundamentals/ConditionsAndBranching.py
@@ -27,3 +27,43 @@ else:
 y=9
 x=1
 print(x>0 and y<10)
+
+############################################
+# ConditionsAndBranching Quiz
+############################################
+
+# Question 1
+# Find the values of i that produces a True for the following:
+# i!=0
+# Possible answers: 1, 0, -1, 10000000
+
+i = 1
+print("Question 1, Answer 1: ", i!=0)
+
+i = 0
+print("Question 1, Answer 2: ", i!=0)
+
+i = -1
+print("Question 1, Answer 3: ", i!=0)
+
+i = 10000000
+print("Question 1, Answer 4: ", i!=0)
+
+# Question 2
+# What is the output of the following:
+# x='a'
+# if(x!='a'):
+# print("this is not a")
+# else:
+# print("this is a")
+
+x='a'
+if(x!='a'):
+    print("this is not a")
+else:
+    print("this is a")
+    
+############################################
+# ConditionsAndBranching Lab
+############################################
+
