959a6885f62e8ed918b45a44a1b01dda77a8fa90
ekmanch <christian.ekman89@gmail.com>
2021-02-13 22:49:37 +0100

    Finished the Numpy 2D Arrays Lab

--- 01 PY0101EN Python Basics for Data Science/Module 5 - Working with Numpy Arrays/Numpy2DArrays.py
+++ 01 PY0101EN Python Basics for Data Science/Module 5 - Working with Numpy Arrays/Numpy2DArrays.py
@@ -65,3 +65,34 @@ B=np.array([[1,2,3],[4,5,6],[7,8,9]])
 # Prediction: You can't. They don't have the correct dimensions (A is 4,2 and B is 3,3)
 
 # np.dot(A,B) <- Uncomment to see error message when running script
+
+#################################################
+# Numpy 2D Arrays Lab
+#################################################
+
+print("LAB")
+
+# Question 1
+# Consider the following list a, convert it to Numpy Array.
+a = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+
+a = np.array(a)
+
+# Question 2
+# Calculate the numpy array size.
+
+print(a.size)
+
+# Question 3
+# Access the element on the first row and first and second columns.
+
+print(a[0][0:2])
+
+# Question 4
+# Perform matrix multiplication with the numpy arrays A and B.
+
+b = np.array([[0, 1], [1, 0], [1, 1], [-1, 0]])
+
+# Answer:
+
+print(np.dot(a,b))
