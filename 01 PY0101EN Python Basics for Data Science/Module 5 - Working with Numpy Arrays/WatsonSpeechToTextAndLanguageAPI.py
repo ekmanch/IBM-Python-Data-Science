c04ad351404031be884ad727084307481a5a7c65
ekmanch <christian.ekman89@gmail.com>
2021-02-14 12:48:55 +0100

    Added WatsonSpeechToTextAndLanguageAPI.py

---
+++ 01 PY0101EN Python Basics for Data Science/Module 5 - Working with Numpy Arrays/WatsonSpeechToTextAndLanguageAPI.py
@@ -0,0 +1,7 @@
+#################################################
+# Watson Speech to Text and Language API Lab
+#################################################
+
+# you will need the following library 
+# !pip install PyJWT==1.7.1 ibm_watson wget
+
