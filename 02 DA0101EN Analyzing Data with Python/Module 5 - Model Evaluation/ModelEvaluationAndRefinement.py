c84ba92d0b1af50ad9f6f0264b20bfad87177959
ekmanch <christian.ekman89@gmail.com>
2021-03-20 12:31:36 +0100

    Completed Question 1 in Lab 5 on Model Evaluation and Refinement

--- 02 DA0101EN Analyzing Data with Python/Module 5 - Model Evaluation/ModelEvaluationAndRefinement.py
+++ 02 DA0101EN Analyzing Data with Python/Module 5 - Model Evaluation/ModelEvaluationAndRefinement.py
@@ -5,8 +5,85 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 # Import clean data 
 path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/module_5_auto.csv'
 df = pd.read_csv(path)
-df.to_csv('module_5_auto.csv')
+# df.to_csv('module_5_auto.csv')    <- Uncomment to save CSV
+
+from ipywidgets import interact, interactive, fixed, interact_manual
+
+# Functions for plotting
+
+def DistributionPlot(RedFunction, BlueFunction, RedName, BlueName, Title):
+    width = 12
+    height = 10
+    plt.figure(figsize=(width, height))
+
+    ax1 = sns.distplot(RedFunction, hist=False, color="r", label=RedName)
+    ax2 = sns.distplot(BlueFunction, hist=False, color="b", label=BlueName, ax=ax1)
+
+    plt.title(Title)
+    plt.xlabel('Price (in dollars)')
+    plt.ylabel('Proportion of Cars')
+
+    plt.show()
+    plt.close()
+
+def PollyPlot(xtrain, xtest, y_train, y_test, lr,poly_transform):
+    width = 12
+    height = 10
+    plt.figure(figsize=(width, height))
+    
+    
+    #training data 
+    #testing data 
+    # lr:  linear regression object 
+    #poly_transform:  polynomial transformation object 
+ 
+    xmax=max([xtrain.values.max(), xtest.values.max()])
+
+    xmin=min([xtrain.values.min(), xtest.values.min()])
+
+    x=np.arange(xmin, xmax, 0.1)
+
+
+    plt.plot(xtrain, y_train, 'ro', label='Training Data')
+    plt.plot(xtest, y_test, 'go', label='Test Data')
+    plt.plot(x, lr.predict(poly_transform.fit_transform(x.reshape(-1, 1))), label='Predicted Function')
+    plt.ylim([-10000, 60000])
+    plt.ylabel('Price')
+    plt.legend()
+
+#%%
+
+# Part 1: Training and Testing
+
+y_data = df['price']
+x_data=df.drop('price',axis=1)
+
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.10, random_state=1)
+
+
+print("number of test samples :", x_test.shape[0])
+print("number of training samples:",x_train.shape[0])
+
+#####################################################################################################
+# Question 1                                                                                        #
+# Use the function "train_test_split" to split up the data set such that 40% of the data samples    #
+# will be utilized for testing, set the parameter "random_state" equal to zero.                     #
+# The output of the function should be the following:                                               #
+# "x_train_1" , "x_test_1", "y_train_1" and "y_test_1".                                             #
+#####################################################################################################
+
+print("\nQuestion 1\n")
+
+x_train_1, x_test_1, y_train_1, y_test_1 = train_test_split(x_data, y_data, test_size=0.4, random_state=0)
+
+print("number of test samples :", x_test_1.shape[0])
+print("number of training samples:",x_train_1.shape[0])
+
